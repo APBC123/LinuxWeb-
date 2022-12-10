dfb9ddc88c3e5404845ca7271cb2471b963f222f
APBC123 <88580735+APBC123@users.noreply.github.com>
2022-12-10 19:05:52 +0800

    Update log.h

--- log/log.h
+++ log/log.h
@@ -1,82 +1,76 @@
 #ifndef LOG_H
 #define LOG_H
 
-#include <assert.h>
-#include <string>
 #include <mutex>
+#include <string>
 #include <thread>
-#include <string.h>
 #include <sys/time.h>
-#include <stdarg.h>
-#include <sys/stat.h>
+#include <string.h>
+#include <stdarg.h>          
+#include <assert.h>
+#include <sys/stat.h>         
 #include "blockqueue.h"
 #include "../buffer/buffer.h"
 
-class Log
-{
+class Log {
 public:
-    void init(int level, const char *path = "./log", const char *suffix = ".log", int maxqueuecapacity = 1024);
-    static Log *instance();
-    static void flushlogthread();
-    void write(int level, const char *format, ...);
-    void flush();
-    int getlevel();
-    void setlevel(int level);
-    bool isopen();
+    void init(int level, const char* path = "./log", 
+                const char* suffix =".log",
+                int maxQueueCapacity = 1024);
 
+    static Log* Instance();
+    static void FlushLogThread();
+
+    void write(int level, const char *format,...);
+    void flush();
+
+    int GetLevel();
+    void SetLevel(int level);
+    bool IsOpen() { return isOpen_; }
+    
 private:
     Log();
-    void AppendLogLevelTitle(int level);
+    void AppendLogLevelTitle_(int level);
     virtual ~Log();
-    void asyncwrite();
+    void AsyncWrite_();
+
+private:
     static const int LOG_PATH_LEN = 256;
     static const int LOG_NAME_LEN = 256;
     static const int MAX_LINES = 50000;
-    const char *m_path;
-    const char *m_suffix;
-    int m_MAX_LINES;
-    int m_linecount;
-    int m_today; //日志日期
-    int m_level;
-    bool m_isopen;
-    bool m_isasync; //异步
-    Buffer m_buffer;
-    FILE *m_fp;
-    std::unique_ptr<BlockDeque<std::string>> m_deque;
-    std::unique_ptr<std::thread> m_writethread;
-    std::mutex m_mtx;
+
+    const char* path_;
+    const char* suffix_;
+
+    int MAX_LINES_;
+
+    int lineCount_;
+    int toDay_;
+
+    bool isOpen_;
+ 
+    Buffer buff_;
+    int level_;
+    bool isAsync_;
+
+    FILE* fp_;
+    std::unique_ptr<BlockDeque<std::string>> deque_; 
+    std::unique_ptr<std::thread> writeThread_;
+    std::mutex mtx_;
 };
 
-#define LOG_BASE(level, format, ...)                   \
-    do                                                 \
-    {                                                  \
-        Log *log = Log::instance();                    \
-        if (log->isopen() && log->getlevel() <= level) \
-        {                                              \
-            log->write(level, format, ##__VA_ARGS__);  \
-            log->flush();                              \
-        }                                              \
-    } while (0);
+#define LOG_BASE(level, format, ...) \
+    do {\
+        Log* log = Log::Instance();\
+        if (log->IsOpen() && log->GetLevel() <= level) {\
+            log->write(level, format, ##__VA_ARGS__); \
+            log->flush();\
+        }\
+    } while(0);
 
-#define LOG_DEBUG(format, ...)             \
-    do                                     \
-    {                                      \
-        LOG_BASE(0, format, ##__VA_ARGS__) \
-    } while (0);
-#define LOG_INFO(format, ...)              \
-    do                                     \
-    {                                      \
-        LOG_BASE(1, format, ##__VA_ARGS__) \
-    } while (0);
-#define LOG_WARN(format, ...)              \
-    do                                     \
-    {                                      \
-        LOG_BASE(2, format, ##__VA_ARGS__) \
-    } while (0);
-#define LOG_ERROR(format, ...)             \
-    do                                     \
-    {                                      \
-        LOG_BASE(3, format, ##__VA_ARGS__) \
-    } while (0);
+#define LOG_DEBUG(format, ...) do {LOG_BASE(0, format, ##__VA_ARGS__)} while(0);
+#define LOG_INFO(format, ...) do {LOG_BASE(1, format, ##__VA_ARGS__)} while(0);
+#define LOG_WARN(format, ...) do {LOG_BASE(2, format, ##__VA_ARGS__)} while(0);
+#define LOG_ERROR(format, ...) do {LOG_BASE(3, format, ##__VA_ARGS__)} while(0);
 
-#endif
+#endif //LOG_H
